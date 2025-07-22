224a8a86383da4abf6c94dc511f3ab5f8591de32
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-07-22 08:55:07 -0500

    feat: create app-header component and integrate it into app.component for display

--- componentes-angular/src/app/app.component.ts
+++ componentes-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from './app-header/app-header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [AppHeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'componentes-angular';
