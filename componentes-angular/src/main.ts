6538b71b17f79ef257e779581576ff6dae8e958e
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2025-08-08 17:04:13 -0500

    feat: configure Spanish localization with LOCALE_ID and add DatePipe for time display formatting

--- componentes-angular/src/main.ts
+++ componentes-angular/src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localEs, 'es');
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
